3b3350367d3539243795b78436ed86762ea6290b
Takayuki Sei <takayuki.sei@gree.net>
2021-04-06 15:05:28 +0900

    initial commit

---
+++ Uaal12.5/Uaal12.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uaal12.5
+//
+//  Created by Takayuki Sei on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
